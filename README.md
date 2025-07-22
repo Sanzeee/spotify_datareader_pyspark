e64854c534a29f475e13476585ea3cd0d6089af5
Sangavi Kumar <68396680+Sanzeee@users.noreply.github.com>
2025-07-22 09:42:33 +0000

    display the tract album and time of play4

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# spotify_datareader_pyspark
-This project is used to extract the most listened songs from spotify 
+# spotify_datareader_pyspark - This project is used to extract the latest listened songs from spotify 
+1.Set up authentication with Spotipy, first create an app here, you will get the client credentials, use them in the secrets.json
+https://developer.spotify.com/dashboard
+
+2.Fetched recent tracks using sp.current_user_recently_played()
+
+Parsed the JSON and flattened nested fields
+
+Converted it to a Spark DataFrame
+
+Explored schema using .printSchema() / .select().show()
+
+Exported it to CSV using df.write.csv()
+
 
 #steps to deploy
 git add .
--- src/extract_spotify_data.py
+++ src/extract_spotify_data.py
@@ -3,6 +3,9 @@ import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import  col
+
+spark = SparkSession.builder.appName("spotify_datareader").getOrCreate()
 
 with open("config/secrets.json","r") as file:
     creds = json.load(file)
@@ -16,13 +19,25 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 
 # https://yourname-yourrepo-xxxxx.github.dev/callback
 
-results = sp.current_user_recently_played(limit=10)
+results = sp.current_user_recently_played()
 results_json= results['items']
 
-for i in results_json:
-    track = i['track']["name"]
-    artists = i['track']["album"]["artists"][0]["name"]
-    print(track)
+
+# print(rdd.first())
+json_string = [json.dumps(record) for record in results_json]
+rdd = spark.sparkContext.parallelize(json_string)
+df = spark.read.json(rdd)
+df.show(1)
+df.printSchema()
+df.select(col("track.name").alias("trackname"),col("track.album.name").alias("albumname"),col("played_at").alias("played_at")).show(truncate=False)
+df.select(col("track.name").alias("trackname"),col("track.album.name").alias("albumname"),col("played_at").alias("played_at")).write.mode("overwrite").option("header" , True).csv("/workspaces/spotify_datareader_pyspark/data/raw/")
+
+# df.printSchema()
+# for i in results_json:
+    
+#     track = i['track']["name"]
+#     artists = i['track']["album"]["artists"][0]["name"]
+    # print(track)
     # for j in track:
     #     songs= (j["external_urls"]["name"])
     #     print(songs)
@@ -32,5 +47,5 @@ for i in results_json:
 
     # items[track[album[artists[external_urls[name
 
-# print(results)
+#print(results)
 
